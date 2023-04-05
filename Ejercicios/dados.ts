c012c78f101b15c3c5089466a5df1851b4de0cfa
sofiazunzunegui <sofiazunzunegui@gmail.com>
2023-04-05 16:55:03 -0300

    ejer dados listo

--- Ejercicios/dados.ts
+++ Ejercicios/dados.ts
@@ -1,13 +1,15 @@
-/*La probabilidad de que un dado de 6 caras salga con un número específico (en este caso, 6) 
-en una sola tirada es de 1/6 o aproximadamente 0.1667.
-La probabilidad de que todos los dados de una tirada de N dados salgan con el número 6 
-es el producto de la probabilidad de que cada dado individual salga con el número 6, 
-ya que estos eventos son independientes entre sí. Por lo tanto, 
-la probabilidad de que todos los N dados muestren 6 es:
-P(Todos los dados muestran 6) = (1/6)^N
-Por ejemplo, si tiras 3 dados, la probabilidad de que todos ellos muestren el número 6 es:
-P(Todos los dados muestran 6) = (1/6)^3 = 0.0046*/
-//a resolver
+/*• Al tirar un dado tenemos 1/6 de
+probabilidades de sacar 6
+• Si tiramos dos dados tenemos 1/36
+probabilidades de sacar doble 6
+• Al aumentar el número de dados la
+probabilidad de sacar todos 6 es cada
+vez menor
+
+• Escriba un programa que calcule la
+probabilidad de sacar todos los dados 6
+siendo que tiramos N dados (dato
+ingresado por el usuario)*/
 import * as rls from "readline-sync";
 let numeroVecesTirarDado: number = rls.questionInt('ingrese numero: ');
 let dado: number = 1/6**numeroVecesTirarDado;
